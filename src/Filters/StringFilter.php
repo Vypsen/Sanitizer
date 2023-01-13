a720fa5002a2737e82b6564dddae6df544a2593d
Vypsen <s.semenets2001@mail.ru>
2023-01-13 20:08:01 +1000

    stringFilter test

--- src/Filters/StringFilter.php
+++ src/Filters/StringFilter.php
@@ -11,13 +11,14 @@ class StringFilter implements Filter
         if (empty($value)) {
             return null;
         }
-        return $value;
+        return (string) $value;
     }
 
     public function validation($value): bool
     {
-        return is_string($value);
+        return is_string($value) || is_int($value) || is_float($value);
     }
+
     public function errorMessageValid(): string
     {
         return "String is not valid";
--- tests/StringFilterTest.php
+++ tests/StringFilterTest.php
@@ -1,53 +1,53 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
-use Vypsen\Sanitizer\Filters\FloatFilter;
+use Vypsen\Sanitizer\Filters\StringFilter;
 use Vypsen\Sanitizer\Sanitizer;
 
-class FloatFilterTest extends TestCase
+class StringFilterTest extends TestCase
 {
-    protected $floatFilter;
+    protected $stringFilter;
     protected $sanitizer;
     protected function setUp(): void
     {
-        $this->floatFilter = new FloatFilter();
+        $this->stringFilter = new StringFilter();
         $this->sanitizer = new Sanitizer();
     }
 
     public function testValidationFail()
     {
-        $value = '123abc';
-        $this->assertSame(false, $this->floatFilter->validation($value));
+        $value = ['1234'];
+        $this->assertSame(false, $this->stringFilter->validation($value));
     }
 
     public function testValidationSuccess()
     {
         $valueStr = '123';
-        $this->assertSame(true, $this->floatFilter->validation($valueStr));
+        $this->assertSame(true, $this->stringFilter->validation($valueStr));
 
-        $valueFloat = 123.0;
-        $this->assertSame(true, $this->floatFilter->validation($valueFloat));
+        $valueInt = 123.0;
+        $this->assertSame(true, $this->stringFilter->validation($valueInt));
     }
 
     public function testSanitizerErrorMessage()
     {
-        $expected = ['value' => $this->floatFilter->errorMessageValid()];
+        $expected = ['value' => $this->stringFilter->errorMessageValid()];
 
-        $data = '{"value": "123abc"}';
-        $filter = ['value' => 'float'];
+        $data = '{"value": ["1234"]}';
+        $filter = ["value" => 'string'];
         $this->assertSame($expected, $this->sanitizer::applySanitizers($data, $filter));
     }
 
     public function testSanitizerSuccess()
     {
-        $expected = ['value' => 123.0];
+        $expected = ['value' => '123'];
 
         $data1 = '{"value": "123"}';
-        $filter1 = ['value' => 'float'];
+        $filter1 = ['value' => 'string'];
         $this->assertSame($expected, $this->sanitizer::applySanitizers($data1, $filter1));
 
         $data2 = '{"value": 123}';
-        $filter2 = ['value' => 'float'];
+        $filter2 = ['value' => 'string'];
         $this->assertSame($expected, $this->sanitizer::applySanitizers($data2, $filter2));
     }
 }
